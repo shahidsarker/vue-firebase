773adfb8a03c4c202dfdfd01ac22dc974f3c310e
Shahid Sarker <shahid.sarker@gmail.com>
2022-01-17 10:39:24 -0500

    conditional rendering

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const app = Vue.createApp({
   // data, functions
-//   template: `<h2>I am the template</h2>`,
+  //   template: `<h2>I am the template</h2>`,
+  data() {
+    return {
+      showBooks: true,
+      title: "The Final Empire",
+      author: "Brandon Sanderson",
+      age: 45,
+    };
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+  },
 });
 
 app.mount("#app");
